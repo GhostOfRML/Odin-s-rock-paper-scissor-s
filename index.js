246e7dc8797df6843dba4a0a18a1804efc9dca78
GhosOfRML <ghostofrml9848@gmail.com>
2026-04-03 15:02:42 +0300

    Added variable getComputerChoice     -This variable gives a number from 1 to 3     -And temporarly converts it to one of the 3     hand signs

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hello World")
+/*Create a function called "getComputerChoice" 
+This function has to randomly pick 1 of 3 choices
+Every 1 of these choices has to concorde with the 
+3 hand signs
+*/
+let getComputerChoice= Math.floor(Math.random()*3)+1;
+    if (getComputerChoice === 1){
+        return ("Rock");
+    }else if (getComputerChoice ===2){
+        return ("Paper");
+    } else {
+        return ("Scissors");
+    }
+
+
