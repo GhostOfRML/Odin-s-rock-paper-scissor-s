d2428b695bbdb76dfd22a4f8e2629f85d12eafca
GhosOfRML <ghostofrml9848@gmail.com>
2026-04-03 15:17:19 +0300

    Added the "getHumanChoice" variable  -This pops up a prompt that asks for a choice Added variable "humanScore" and "computerScore"  -Which will be used to hold the scores

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-/*Create a function called "getComputerChoice" 
+/*1.Create a variable called "getComputerChoice" 
 This function has to randomly pick 1 of 3 choices
-Every 1 of these choices has to concorde with the 
-3 hand signs
+Every 1 of these numbers has to concorde with the 
+three choices
+2.Make a variable called "getHumanChoice"
+This variable stores the choice of te user
+3.Create two variables "humanScore" and "computerScore"
+This two should store the score each side gets every round
 */
 let getComputerChoice= Math.floor(Math.random()*3)+1;
     if (getComputerChoice === 1){
-        return ("Rock");
+        console.log ("Rock");
     }else if (getComputerChoice ===2){
-        return ("Paper");
+        console.log("Paper") ;
     } else {
-        return ("Scissors");
+        console.log ("Scissors");
     }
-
+let getHumnaChoice=prompt("What is your choice?")
+let humanScore ="";
+let computerScore="";
 
